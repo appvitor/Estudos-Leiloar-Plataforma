906273d621e9cf24ab153e6f6d868c449b0c1bf9
Vítor <vitorcorrea11@hotmail.com>
2019-02-11 14:59:32 -0300

    Criado pessoasArray para manipular JSON
    Criado arquivo arrayPessoas p/ estudar e aprender JSON

---
+++ arrayPessoas.php
@@ -0,0 +1,7 @@
+<?
+	$pessoas = array();
+	array_push($pessoas, array('nome'=>'João', 'idade'=>20));
+	array_push($pessoas, array('nome'=>'Glaucio', 'idade'=>25));
+
+	echo json_encode($pessoas);
+?>

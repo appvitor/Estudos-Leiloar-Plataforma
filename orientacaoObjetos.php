bcbcbcf9c5f8db1d6ad1a9443638e1089829ebbc
Vítor <vitorcorrea11@hotmail.com>
2019-02-13 11:59:48 -0300

    Update orientacaoObjetos.php
    Geters e seters, class Carro

--- orientacaoObjetos.php
+++ orientacaoObjetos.php
@@ -3,7 +3,7 @@
 	setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "portuguese");
 
 	class Pessoa {
-		public $nome; //atributos públicos
+		public $nome, $idade; //atributos públicos
 
 		public function falarNome(){ //Método
 			return '<br>'.'O meu nome é '.$this->nome;
@@ -13,4 +13,49 @@
 	$joao = new Pessoa();
 	$joao->nome = "Joao Pedro";
 	echo $joao->falarNome();
+
+	class Carro {
+		private $modelo;
+		private $motor;
+		private $ano;
+
+		public function getModelo(){
+			return $this->modelo;
+		}
+
+		public function setModelo($receberModelo) {
+			$this->modelo = $receberModelo;
+		}
+
+		public function getMotor(){
+			return $this->motor;
+		}
+
+		public function setMotor($receberMotor) {
+			$this->motor = $receberMotor;
+		}
+
+		public function getAno(){
+			return $this->ano;
+		}
+
+		public function setAno($receberAno) {
+			$this->ano = $receberAno;
+		}
+
+		public function exibir(){
+			return array(
+				'modelo'=>$this->getModelo(),
+				'motor'=>$this->getMotor(),
+				'ano'=>$this->getAno()
+			);
+		}
+	}
+
+	$veloster = new Carro();
+	$veloster->setModelo("Veloster");
+	$veloster->setMotor("1.6 Turbo");
+	$veloster->setAno(2016);
+	echo '<br>';
+	print_r($veloster->exibir());
 ?>
